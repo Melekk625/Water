490281415fbc893ffcce79d9629df5094f2390a8
Melekk625 <memmedovamelek415@gmail.com>
2025-05-18 17:52:03 +0400

    first5

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,50 @@
-const main = document.querySelector(".main");
-const boxList = document.querySelectorAll(".box");
+const smallCups = document.querySelectorAll('.cup.small');
+const liters = document.getElementById('liters');
+const percentage = document.getElementById('percentage');
+const remained = document.getElementById('remained');
 
-boxList.forEach(b => {
-    b.addEventListener("click", function() {
-        let existingDiv = document.querySelector(".toggle-div");
+function updateBigCup() {
+  const fullCups = document.querySelectorAll('.cup.small.full').length;
+  const totalCups = smallCups.length;
 
-        if (existingDiv) {
-            existingDiv.remove();
-        } else {
-            b.style.color = "#144fc6";
-            let div = document.createElement("div");
-            div.classList.add("toggle-div");
-            div.style.height = "12.5%";
-            div.style.width = "100%";
-            div.style.position = "absolute";
-            div.style.bottom = "0";
-            div.style.borderRadius = "1px 1px 20px 20px";
-            div.style.backgroundColor = "#144fc6";
-            main.append(div);
-            
-        }
-    });
+  if (fullCups === 0) {
+    percentage.style.height = '0';
+    percentage.innerText = '';
+    remained.style.visibility = 'visible';
+    liters.innerText = '2L';
+  } else {
+    const percent = (fullCups / totalCups) * 100;
+    percentage.style.height = `${percent}%`;
+    percentage.innerText = `${percent}%`;
+    remained.style.visibility = fullCups === totalCups ? 'hidden' : 'visible';
+    liters.innerText = `${(2 - (fullCups * 0.25)).toFixed(2)}L`;
+  }
+}
+
+function highlightCups(idx) {
+  // Əgər kliklənən qab doludursa və növbəti qab boşdursa, idx azaldırıq (geri çəkmə)
+  if (
+    smallCups[idx].classList.contains('full') &&
+    !smallCups[idx].nextElementSibling?.classList.contains('full')
+  ) {
+    idx--;
+  }
+
+  smallCups.forEach((cup, i) => {
+    if (i <= idx) {
+      cup.classList.add('full');
+    } else {
+      cup.classList.remove('full');
+    }
+  });
+
+  updateBigCup();
+}
+
+// Başlanğıcda big cup-u yenilə
+updateBigCup();
+
+smallCups.forEach((cup, idx) => {
+  cup.addEventListener('click', () => highlightCups(idx));
 });
+
